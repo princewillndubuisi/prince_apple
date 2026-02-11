a12c6713e41d20ab6fca45e746b53466f4ad50a7
Princewill Chidi <princewillchidi107@gmail.com>
2026-02-11 13:08:37 +0100

    3D Product Ui Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import ProductUi from "./components/ProductUi";
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Hero />
+      <ProductUi />
     </main>
   );
 };
