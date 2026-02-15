7d6951fd0f19c248e78045e162bb923ff22299a6
Princewill Chidi <princewillchidi107@gmail.com>
2026-02-15 21:21:42 +0100

    add a hypen

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -13,7 +13,7 @@ import gsap from "gsap";
 
 const ModelScroll = () => {
   const groupRef = useRef(null);
-  const isMobile = useMediaQuery({ query: "(max width: 1024px)" });
+  const isMobile = useMediaQuery({ query: "(max-width: 1024px)" });
   const { setTexture } = useMacbookStore();
 
   useEffect(() => {
